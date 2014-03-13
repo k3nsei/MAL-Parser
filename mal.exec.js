ad55e6ddae80775412b508d08e09467f37451f25
k3nsei <k3nsei.pl@gmail.com>
2014-03-13 22:35:54 +0100

    Create mal.exec.js

---
+++ mal.exec.js
@@ -0,0 +1,36 @@
+/*!
+ * MyAnimeList Data Collector application based at CasperJS.
+ *
+ * Version: 1.0.0
+ * Usage: node mal.exec.js --url "http://myanimelist.net/manga/{id}"
+ *
+ * Copyright (c) 2014 k3nsei.pl@gmail.com
+ *
+ */
+var stdio = require('stdio');
+var exec = require('child_process').exec,
+	child;
+
+var ops = stdio.getopt({
+	'url': {key: 'u', args: 1, mandatory: true},
+	'ooo': {key: 'o'}
+});
+
+var siteUrl = null;
+
+if (ops.url) {
+	siteUrl = ops.url;
+	var urlPattern = siteUrl.match(/http:\/\/(www\.|)myanimelist\.net\/manga\/(\d+)/);
+	if (urlPattern !== null) {
+		child = exec('casperjs mal.casper.js ' + siteUrl,
+			function (error, stdout) {
+				console.log(stdout);
+				if (error !== null) {
+					console.log('exec error: ' + error);
+				}
+			}
+		);
+	} else {
+		console.log("Invalid url...");
+	}
+}
